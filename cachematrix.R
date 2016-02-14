c703ad375b2f166d2189d29012dde9e608408adc
Jose Carlos Arriola <josecarlos@Jose.telecom>
2016-02-14 21:55:58 +1300

    Improving Rmd file

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Lexical Scoping
 
-## Write a short comment describing this function
+# Matrix inversion is usually a costly computation and there may be some benefit to
+# caching the inverse of a matrix rather than compute it repeatedly (there are also 
+# alternatives to matrix inversion that we will not discuss here). In this assignment 
+# a pair of functions are implemented that cache the inverse of a matrix: 
+# makeCacheMatrix and cacheSolve.
+
+# Creates a special "matrix", which is really a list containing a function to
+#
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse matrix
+# 4. get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -17,8 +27,9 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# Computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
